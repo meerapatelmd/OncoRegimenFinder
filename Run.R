335bd9845c0b1c276cbefba9493ec7a1c70c508f
Meera Patel <patelm9@mskcc.org>
2020-07-28 19:06:38 -0400

    update functions

--- Run.R
+++ Run.R
@@ -1,14 +1,14 @@
-settings <-
-        list(cdmDatabaseSchema = "omop_cdm_2",
-             writeDatabaseSchema = "patelm9",
-             cohortTable = "oncoregimenfinder_cohort",
-             regimenTable = "oncoregimenfinder_regimen",
-             vocabularyTable = "oncoregimenfinder_vocabulary",
-             regimenIngredientTable= "oncoregimenfinder_regimen_ingredients",
-             drug_classification_id_input = c(21601387,
-                                              35807188,
-                                              35807277,
-                                              35807189),
-             date_lag_input = 30,
-             regimen_repeats = 5)
+# settings <-
+#         list(cdmDatabaseSchema = "omop_cdm_2",
+#              writeDatabaseSchema = "patelm9",
+#              cohortTable = "oncoregimenfinder_cohort",
+#              regimenTable = "oncoregimenfinder_regimen",
+#              vocabularyTable = "oncoregimenfinder_vocabulary",
+#              regimenIngredientTable= "oncoregimenfinder_regimen_ingredients",
+#              drug_classification_id_input = c(21601387,
+#                                               35807188,
+#                                               35807277,
+#                                               35807189),
+#              date_lag_input = 30,
+#              regimen_repeats = 5)
 
