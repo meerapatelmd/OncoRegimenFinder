d187cfd6c3ee9e7322eca309545b9924d57b2e48
Meera Patel <patelm9@mskcc.org>
2020-08-08 17:09:57 -0400

    bug fix vocab

--- R/Run2.R
+++ R/Run2.R
@@ -1,16 +1,24 @@
-
+# 
 # vocabulary <-
 #         fantasia::qOMOP(pg13::buildQuery(schema = "patelm9",
 #                                          tableName = "oncoregimenfinder_vocabulary"),
 #                         override_cache = T)
 # 
-# vocabulary %>% 
-#         dplyr::arrange(ingredient_name) %>% 
+# vocabulary %>%
+#         dplyr::select(regimen_id,
+#                       ingredient_id,
+#                       ingredient_name) %>%
+#         dplyr::distinct() %>%
+#         group_by(ingredient_id) %>%
+#         dplyr::mutate(ingr_name_count = length(unique(ingredient_name))) %>%
+#         dplyr::arrange(desc(ingr_name_count))
+# 
+#         dplyr::arrange(ingredient_name) %>%
 #         rubix::group_by_unique_aggregate(regimen_id, regimen_name,
 #                                   agg.col = ingredient_name,
 #                                   collapse = ", ") %>%
 #         dplyr::rename(ingredient_combination = ingredient_name) %>%
-#         group_by(ingredient_combination) %>% 
-#         mutate(unique_ingr_comb_count = length(unique(regimen_id))) %>% 
+#         group_by(ingredient_combination) %>%
+#         mutate(unique_ingr_comb_count = length(unique(regimen_id))) %>%
 #         dplyr::filter(unique_ingr_comb_count > 1) %>%
 #         dplyr::arrange(desc(unique_ingr_comb_count))
