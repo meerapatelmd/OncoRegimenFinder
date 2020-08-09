df8edb1b6d139a78d80825a1415c35c335541bdc
Meera Patel <patelm9@mskcc.org>
2020-08-09 03:00:17 -0400

    add readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# OncoRegimenFinder <img src="man/figures/logo.png" align="right" alt="" width="120" />  
+This package extracts Chemotherapy Regimens from the OMOP CDM Drug Exposures Table based on an algorithm developed by the OHDSI Oncology Workgroup. 
+
---
+++ vignettes/OncoRegimenFinder.R
@@ -0,0 +1,11 @@
+## ---- include = FALSE---------------------------------------------------------
+knitr::opts_chunk$set(
+  tidy = 'styler',
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ----pkgsetup, echo=TRUE,eval=TRUE,message=FALSE,results="hide", warning=FALSE----
+library(OncoRegimenFinder)
+library(tidyverse)
+
