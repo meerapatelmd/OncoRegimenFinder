45a10ba9ae445ac1bfb45b5f0f35157781913668
Meera Patel <patelm9@mskcc.org>
2020-08-21 10:02:42 -0400

    update Run.R with connection object

--- Run.R
+++ Run.R
@@ -1,3 +1,12 @@
+# Connection Object
+library(DatabaseConnector)
+conn_details <- DatabaseConnector::createConnectionDetails(dbms = "postgresql",
+                                                           user, 
+                                                           password,
+                                                           port = "5432", 
+                                                           server)
+conn <- DatabaseConnector::connect(conn_details)
+
 # Settings
 cdmDatabaseSchema <- "omop_cdm_2"
 writeDatabaseSchema <- "oncoregimenfinder"
